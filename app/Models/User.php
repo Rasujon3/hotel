f9bea2af8c0c32128e85b967be5ac2db0da60204
Rasujon3 <rasujon3@gmail.com>
2025-09-02 23:49:20 +0600

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Support\Facades\Log;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'username',
+        'password',
+        'otp',
+        'otp_enabled',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'otp',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'otp_enabled' => 'boolean',
+        ];
+    }
+
+    /**
+     * Generate and send OTP (mock implementation).
+     */
+    public function sendOtp()
+    {
+        $otp = random_int(100000, 999999);
+        $this->update(['otp' => $otp]);
+
+        Log::info("OTP for {$this->username}: {$otp}");
+    }
+}
