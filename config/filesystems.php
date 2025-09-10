91766ed9a48b15918ce90c12c3f6ec19c3ea814a
Sujon Office <>
2025-09-10 17:26:45 +0600

    profile pic upload s3 5

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,8 +50,8 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION', "ap-south-1"),
-            'bucket' => env('AWS_BUCKET', "aisoft-hotel-project"),
-            'url' => env('AWS_URL', "https://aisoft-hotel-project.s3.ap-south-1.amazonaws.com/"),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'), // optional for custom endpoints
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
